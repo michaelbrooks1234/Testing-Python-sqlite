4dfa6ebb583786f814f54cb77e4ecba8014c084c
Michael <michaelbrooks6897@gmail.com>
2022-09-17 10:26:57 -0400

    testing tkinter

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from src.setup import *
-
+from tkinter import *
 
 def main():
     
@@ -15,3 +15,27 @@ def main():
 
 
 main()
+
+height = 600
+width = 800
+
+root = Tk()
+root.geometry(f"{width}x{height}")
+root.resizable(False, False)
+
+canvas = Canvas(root, height=height, width=width)
+canvas.pack()
+
+frame = Frame(root)
+frame.place(relheight=1, relwidth=1)
+
+for i in range(10):
+    container = Frame(frame)
+    container.grid(column=0, row=i)
+    label = Label(container, text="hello World!", height=2, width=10)
+    label.grid(column=0, row=0)
+    button = Button(container, text="quit", command=root.destroy, height=1, width=5)
+    button.grid(column=1, row=0)
+
+
+root.mainloop()
